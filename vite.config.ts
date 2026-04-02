6dcad67d0720e2d22a6bf7357a031d3aab7ab98a
Matheus Sathler <mattsathler@gmail.com>
2026-04-02 19:34:46 -0300

    chore(npm): create build modes for vercel & npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-  build: {
-    lib: {
-      entry: "src/lib/index.ts",
-      name: "LuvUI",
-      fileName: "luv-ui",
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-    },
+export default defineConfig(({ mode }) => {
+  if (mode === "lib") {
+    return {
+      plugins: [react()],
+      build: {
+        lib: {
+          entry: "src/lib/index.ts",
+          name: "LuvUI",
+          fileName: "luv-ui",
+        },
+        rollupOptions: {
+          external: ["react", "react-dom"],
+        },
+      },
+    }
+  }
+
+  return {
+    plugins: [react()],
   }
 })
